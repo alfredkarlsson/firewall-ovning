e5b6690104b87034dbdd108f2238077e2d18bfeb
alfredkarlsson <45688796+alfredkarlsson@users.noreply.github.com>
2020-10-22 10:35:40 +0200

    firewall
    uppgift om firewall som du vile ha

---
+++ firewall,ovn.ps1
@@ -0,0 +1,6 @@
+﻿$portar = import-csv -delimiter ";" -path "C:\code\powershell\firewall,ovn\firewall,ovn.csv"
+
+foreach ($port in $portar) {
+    New-NetFirewallRule -DisplayName $port.protokoll -Direction Outbound -Protocol tcp -LocalPort $port.port -Action $port.regel
+    write-host "porten" $port.port "är nu" $port.regel
+}
